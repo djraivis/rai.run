ff21a1eff9b9caae2a6885c1922dbce1bb257afd
Raivis Gereiss <highdoven@gmail.com>
2023-09-30 22:27:55 +0100

    Delete files and info changed new user

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://uses.craftz.dog/',
+  site: 'https://timlesscoding.com',
   integrations: [
     sitemap(),
     react(),
